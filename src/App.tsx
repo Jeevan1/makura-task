ad34f1c3c862d5842eee88e77e2d8d4b667a483c
Jeevan <shresthaj1986@gmail.com>
2025-11-10 08:52:46 +0545

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./styles/App.css";
-
 import { createBrowserRouter, RouterProvider } from "react-router";
 import MainLayout from "./components/layout/MainLayout";
 import HomePage from "./pages/HomePage";
+
+import "./styles/App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   const router = createBrowserRouter([
